5e4ff683e0a510fca43c9c076b19d2dae7a54c14
unknown <herew26@gmail.com>
2020-03-02 00:59:03 +0300

    Console font added

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
-console_text = '1'
-user_mc_version = '1.12.2'
-user_mc_path = ''
+console_text = ''
+user_mc_version = '1.12.1'
+user_mc_path = 'C:/Users/Tim PC/AppData/Roaming/.minecraft/mods'
+console_font = '13'
