4f36c35f8c619d51107d96c8aa6c3eed1ecb41af
unknown <herew26@gmail.com>
2020-03-10 22:11:58 +0300

    New type of data

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 console_text = ''
-user_mc_version = '1.15.2'
-user_mc_path = ''
+user_mc_version = '1.12.2'
+user_mc_path = r'C:\Users\Tim PC\AppData\Roaming\.minecraft\mods'
 console_font = '8'
 save_old_mods = False
 dark_mode = True
